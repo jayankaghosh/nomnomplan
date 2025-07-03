67d6f701c1c396aa9bc3cf24a2f69dd504e79562
Jayanka Ghosh <joy@codilar.com>
2025-07-03 18:44:26 +0530

    user schedule added

--- src/Console/DataImport.php
+++ src/Console/DataImport.php
@@ -2,9 +2,7 @@
 
 namespace JayankaGhosh\NomNomPlan\Console;
 
-use Algolia\AlgoliaSearch\Api\SearchClient;
 use JayankaGhosh\NomNomPlan\Exception\ApplicationException;
-use JayankaGhosh\NomNomPlan\Model\TableFactory;
 use JayankaGhosh\NomNomPlan\Util\Import;
 use Om\DependencyInjection\NonInterceptableInterface;
 use Symfony\Component\Console\Command\Command;
@@ -16,7 +14,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 class DataImport extends Command implements NonInterceptableInterface
 {
     public function __construct(
-        private readonly TableFactory $tableFactory,
         private readonly Import $importUtil,
         ?string $name = null
     )
@@ -68,56 +65,6 @@ class DataImport extends Command implements NonInterceptableInterface
         $output->writeln('');
         $output->writeln('<info>Import completed successfully</info>');
         return self::SUCCESS;
-    }
-
-
-    private function prepareRecipe(array $recipe): array
-    {
-        $table = $this->tableFactory->create(['tableName' => 'recipe_ingredient']);
-        $ingredients = $table->select(
-            [
-                'recipe_ingredient.recipe_id' => $recipe['id']
-            ],
-            [
-                'ingredient.id',
-                'ingredient.name',
-                'ingredient.is_veg',
-                'ingredient.qty_unit',
-                'ingredient.created_at',
-                'ingredient.updated_at',
-                'recipe_ingredient.ingredient_qty(qty)'
-            ],
-            [
-                '[>]ingredient' => ['ingredient_id' => 'id']
-            ]
-        );
-        $recipe['ingredients'] = $ingredients;
-        return $recipe;
-    }
-
-    private function indexByPage(string $tableName, int $pageSize = 500, ?\Closure $handler = null): void
-    {
-        $table = $this->tableFactory->create(['tableName' => $tableName]);
-        $count = $table->count([]);
-        $pages = ceil($count/$pageSize);
-        for ($page = 1; $page <= $pages; $page++) {
-            $rows = $table->select([
-                'LIMIT' => [$pageSize * ($page - 1), $pageSize]
-            ]);
-            $data = [];
-            foreach ($rows as $row) {
-                if ($handler) {
-                    $row = $handler($row);
-                }
-                $row['objectID'] = $row['id'];
-                $data[] = $row;
-            }
-            $client = SearchClient::create($_ENV['ALGOLIA_APP_ID'], $_ENV['ALGOLIA_API_KEY']);
-            $client->saveObjects(
-                $tableName . '_index',
-                $data,
-            );
-        }
     }
 
 }
